9fe2744835751c8c65ebfb595bfb5ac4a7752bd6
Aigul Mustafina <aimustafina2016@gmail.com>
2022-10-09 17:39:38 +0600

    Completed tasks on various types

---
+++ variousTypes.js
@@ -0,0 +1,30 @@
+let num1 = 10
+let str1 = 'Hello'
+let bool1 = true
+let res1 = num1 + str1
+let res2 = str1 + bool1
+let res3 = num1 + bool1
+console.log("res1 =", res1)
+console.log("res2 =", res2)
+console.log("res3 =", res3)
+
+
+let num2 = 10
+let str2 = 'Hello'
+let bool2 = true
+let res4 = str2 * num2
+let res5 = str2 * bool2
+let res6 = num2 * bool2
+console.log("res4 =", res4)
+console.log("res5 =", res5)
+console.log("res6 =", res6)
+
+let num3 = 10
+let str3 = 'Hello'
+let bool3 = true
+let res7 = num3 / str3
+let res8 = str3 / bool3
+let res9 = num3 / bool3
+console.log("res7 =", res7)
+console.log("res8 =", res8)
+console.log("res9 =", res9)
