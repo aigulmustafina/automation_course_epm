d2af535402d10f4a1687184905c09042b63b3a61
Aigul Mustafina <aimustafina2016@gmail.com>
2022-10-15 21:24:49 +0600

    Added throw error on 1/0 task

--- exceptionHandling.js
+++ exceptionHandling.js
@@ -2,18 +2,15 @@
 try {
     console.log(a)
     let a = 3
-}
-
-catch(ReferenceError) {
+} catch(ReferenceError) {
     console.log('let must be declared')
 }
 
 // task2
 let result = 1 / 0
 if (!isFinite(result)) {
-    console.log('cannot be divided by zero')
-}
-else {
+    throw new Error('cannot be divided by zero')
+} else {
     console.log(result)
 }
     
