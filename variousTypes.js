4b409fc358dc1f3f6b66140aa790081bd6c6f52a
Aigul Mustafina <aimustafina2016@gmail.com>
2022-10-15 21:04:09 +0600

    Added explicit type conversion

--- variousTypes.js
+++ variousTypes.js
@@ -1,30 +1,36 @@
-let num1 = 10
-let str1 = 'Hello'
-let bool1 = true
-let res1 = num1 + str1
-let res2 = str1 + bool1
-let res3 = num1 + bool1
-console.log("res1 =", res1)
-console.log("res2 =", res2)
-console.log("res3 =", res3)
+// implicite conversion
+let num1 = 10;
+let str1 = 'Hello';
+let bool1 = true;
+let res1 = str1 + bool1;
+let res2 = str1 + num1;
+let res3 = num1 + bool1;
+console.log("res1 =", res1, "res2 =", res2, "res3 =", res3)
+
+let res4 = str1 * bool1;
+let res5 = str1 * num1;
+let res6 = num1 * bool1;
+console.log("res4 =", res4, "res5 =", res5, "res6 =", res6)
+
+let res7 = str1/ bool1;
+let res8 = str1 / num1;
+let res9 = num1 / bool1;
+console.log("res7 =", res7, "res8 =", res8, "res9 =", res9)
 
 
-let num2 = 10
-let str2 = 'Hello'
-let bool2 = true
-let res4 = str2 * num2
-let res5 = str2 * bool2
-let res6 = num2 * bool2
-console.log("res4 =", res4)
-console.log("res5 =", res5)
-console.log("res6 =", res6)
+// expicit conversion
+let res10 = Boolean(str1) + bool1;
+let res11 = str1 + String(num1);
+let res12 = num1 + Number(bool1);
+console.log("res10 =", res10, "res11 =", res11, "res12 =", res12)
 
-let num3 = 10
-let str3 = 'Hello'
-let bool3 = true
-let res7 = num3 / str3
-let res8 = str3 / bool3
-let res9 = num3 / bool3
-console.log("res7 =", res7)
-console.log("res8 =", res8)
-console.log("res9 =", res9)
+let res13 = Boolean(str1) * bool1;
+let res14 = Boolean(str1) * num1;
+let res15 = num1 * Number(bool1);
+console.log("res13 =", res13, "res14 =", res14, "res15 =", res15)
+
+
+let res16 = Boolean(str1) / bool1;
+let res17 = num1 / Boolean(str1);
+let res18 = num1 / Number(bool1);
+console.log("res16 =", res16, "res17 =", res17, "res18 =", res18)
